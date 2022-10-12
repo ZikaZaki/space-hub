c8224d9dfe3a296fa2c63b73ca27c68aa81265e6
ZikaZaki <zika.github@gmail.com>
2022-10-13 00:19:10 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 import RocketList from './components/RocketList/RocketList';
 import ProfilePage from './components/ProfilePage/ProfilePage';
+import Navbar from './components/Navbar/Navbar';
 
 // import styling
 import styles from './App.module.css';
